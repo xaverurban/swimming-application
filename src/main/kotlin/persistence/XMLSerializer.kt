78bd325295dd4f5f9c77ec026de12c9383495874
xaverurban <urbanxaver@hotmail.com>
2023-04-24 12:25:25 +0100

    -Fix missing files

---
+++ src/main/kotlin/persistence/XMLSerializer.kt
@@ -0,0 +1,34 @@
+package persistence
+
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.xml.DomDriver
+import models.Swimmer
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
+
+
+    class XMLSerializer(private val file: File) : Serializer {
+
+        @Throws(Exception::class)
+        override fun read(): Any {
+            val xStream = XStream(DomDriver())
+            xStream.allowTypes(arrayOf(Swimmer::class.java))
+            val inputStream = xStream.createObjectInputStream(FileReader(file))
+            val obj = inputStream.readObject() as Any
+            inputStream.close()
+            return obj
+        }
+
+
+        @Throws(Exception::class)
+        override fun write(obj: Any?) {
+            val xStream = XStream(DomDriver())
+            val outputStream = xStream.createObjectOutputStream(FileWriter(file))
+            outputStream.writeObject(obj)
+            outputStream.close()
+        }
+    }
+
+
