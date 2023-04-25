6fcccb286b0815a8af3657786ed416d8891d430b
xaverurban <urbanxaver@hotmail.com>
2023-04-25 16:55:53 +0100

    -Add documentation for Utilities -Add documentation for XML Serializer -Generated webpage using KDoc and DokkaHTML

--- src/main/kotlin/persistence/XMLSerializer.kt
+++ src/main/kotlin/persistence/XMLSerializer.kt
@@ -8,8 +8,19 @@ import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 
+/**
+ * XMLSerializer is a class that implements the Serializer interface for reading and writing
+ * Swimmer and Race objects to and from an XML file.
+ *
+ * @property file The file where the serialized data will be stored.
+ */
 class XMLSerializer(private val file: File) : Serializer {
 
+    /**
+     * Reads serialized Swimmer and Race objects from the XML file.
+     * @return Deserialized Swimmer and Race objects.
+     * @throws Exception If an error occurs during reading the file or deserializing the objects.
+     */
     @Throws(Exception::class)
     override fun read(): Any {
         val xStream = XStream(DomDriver())
@@ -20,6 +31,11 @@ class XMLSerializer(private val file: File) : Serializer {
         return obj
     }
 
+    /**
+     * Writes serialized Swimmer and Race objects to the XML file.
+     * @param obj The Swimmer and Race objects to be serialized.
+     * @throws Exception If an error occurs during writing to the file or serializing the objects.
+     */
     @Throws(Exception::class)
     override fun write(obj: Any?) {
         val xStream = XStream(DomDriver())
--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -3,13 +3,26 @@ package utils
 import models.Race
 import models.Swimmer
 
+/**
+ * Utilities is a singleton object that provides utility methods for formatting lists and sets of Swimmer and Race objects.
+ */
 object Utilities {
 
+    /**
+     * Formats a list of Swimmer objects into a readable string.
+     * @param notesToFormat The list of Swimmer objects to format.
+     * @return A formatted string representing the list of Swimmer objects.
+     */
     @JvmStatic
     fun formatListString(notesToFormat: List<Swimmer>): String =
         notesToFormat
             .joinToString(separator = "\n") { swimmer ->  "$swimmer" }
 
+    /**
+     * Formats a set of Race objects into a readable string.
+     * @param itemsToFormat The set of Race objects to format.
+     * @return A formatted string representing the set of Race objects.
+     */
     @JvmStatic
     fun formatSetString(itemsToFormat: Set<Race>): String =
         itemsToFormat
