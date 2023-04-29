4fefb45eb5a5b154abfb4cf6bb032f8bdc14362b
xaverurban <urbanxaver@hotmail.com>
2023-04-29 20:32:17 +0100

    -Potential workflow fix

--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -1,14 +1,11 @@
 package utils
 
 import models.Race
-import models.Swimmer
 
 /**
  * Utilities is a singleton object that provides utility methods for formatting lists and sets of Swimmer and Race objects.
  */
 object Utilities {
-
-
     /**
      * Formats a set of Race objects into a readable string.
      * @param itemsToFormat The set of Race objects to format.
@@ -18,5 +15,4 @@ object Utilities {
     fun formatSetString(itemsToFormat: Set<Race>): String =
         itemsToFormat
             .joinToString(separator = "\n") { race -> "\t$race" }
-
 }
